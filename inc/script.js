b2203e882eb5dc0396f376b83748f9f3acac957c
Brooks Boyd <boydb@midnightdesign.ws>
2012-04-04 15:57:41 -0500

    Animated SVG loading images

--- inc/script.js
+++ inc/script.js
@@ -4,6 +4,32 @@ $(document).ready(function() {
 	
 	$('div#left_col').height(windowHeight-20).jScrollPane();
 	$('div#right_col').height(windowHeight-20).jScrollPane();
+	
+	$('.loader')
+		.css('opacity', 0)
+		.on('ajaxStart', function(e) {
+			console.log('Start');
+			$(this).css('opacity', 1);
+		})
+		.on('ajaxStop', function(e) {
+			console.log('Stop');
+			$(this).css('opacity', 0);
+		});
+	$('button#testing').on('click', function(e) {
+		console.log('AJAX test');
+		$.ajax({
+			url: 'crud.php',
+			type: 'POST',
+			data: {
+				grimoire: 'adfwersbaer',
+				name: 'NULL',
+				reference: '23434'
+			},
+			success: function(data, status, xhr) {
+				console.log(data);
+			}
+		});
+	});
 });
 
 $(window).resize(function() {

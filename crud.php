745c5813df6071de0952de2cdaca78f40102f7cb
Brooks Boyd <boydb@midnightdesign.ws>
2012-04-04 15:57:08 -0500

    CRUD progress

--- crud.php
+++ crud.php
@@ -1,19 +1,20 @@
 <?php
 // Create/Read/Edit/Delete (CRUD) Ajax handler'
 $pk_var = 'id'; // Which request variable is the primary key being acted upon?
+$table_name = 'data'; // Name of database table to act upon?
 
 $verb = $_SERVER['REQUEST_METHOD'];
 switch($verb) {
 	case 'POST':
 		if (isset($_POST[$pk_var])) {
-			do_update();
+			do_update($pk_var, $table_name);
 		} else {
-			do_create();
+			do_create($table_name);
 		}
 	case 'GET':
-		do_read();
+		do_read($pk_var, $table_name);
 	case 'DELETE':
-		do_delete();
+		do_delete($pk_var, $table_name);
 	default:
 		fail_out(400, 'Action not recognized');
 }
@@ -24,16 +25,50 @@ function db() {
 	return $db;
 }
 
-function do_read() {
-	global $pk_var;
+function do_read($pk_var, $table_name) {
 	if (!isset($_GET[$pk_var])) { fail_out(400, 'No ID specified'); }
 	$db = db(); // Get PDO object
-	$stmt = $db->prepare('SELECT * FROM "data" WHERE ROWID=?');
+	$stmt = $db->prepare('SELECT * FROM "'.$table_name.'" WHERE ROWID=?');
 	$stmt->execute(array($_GET[$pk_var]));
 	$data = $stmt->fetch(PDO::FETCH_OBJ);
 	$out = new responseObject();
 	$out->data = $data;
 	echo json_encode($out);
+	exit;
+}
+
+function do_create($table_name) {
+	// Every value in the $_POST array is a column in the database; insert that data into a row
+	$keys = array_keys($_POST);
+	$sql = 'INSERT INTO "'.$table_name.'" (';
+	foreach($keys as $key) {
+		$sql .= '"'.$key.'", ';
+	}
+	$sql = substr($sql, 0,-2).') VALUES (';
+	foreach($keys as $key) {
+		$sql .= '?, ';
+	}
+	$sql = substr($sql,0,-2).')';
+	
+	$db = db(); // Get PDO object
+	$stmt = $db->prepare($sql);
+	if (!$stmt) { fail_out(500, 'SQL error: '.$sql.': '.$db->errorInfo()); }
+	foreach($keys as $i => $key) {
+		if (!is_numeric($_POST[$key]) && $_POST[$key] != 'NULL') {
+			$stmt->bindValue($i+1, $_POST[$key], PDO::PARAM_STR);
+		} elseif ($_POST[$key] == 'NULL') {
+			$stmt->bindValue($i+1, $_POST[$key], PDO::PARAM_NULL);
+		} else {
+			$stmt->bindValue($i+1, $_POST[$key]);
+		}
+	}
+	$stmt->execute();
+	if ($stmt->rowCount() < 1) { fail_out(500, 'SQL Failed insert: '.var_dump($db->errorInfo(), true)); }
+	$out = new responseObject();
+	$out->id = $db->lastInsertId();
+	echo json_encode($out);
+	sleep(3);
+	exit;
 }
 
 function fail_out($err_no, $message) {
